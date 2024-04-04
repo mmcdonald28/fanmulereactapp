f26f9bc09a3a4c5e1d4502aefc8362868c3b9d28
mmcdonald28 <mmcdonald@muhlenberg.edu>
2024-04-03 20:25:42 -0400

    fixed merge error that overrode everything

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
 import './App.css';
-import GoodSignUp from './ui-components/GoodSignUp';
+//import GoodSignUp from './ui-components/GoodSignUp';
+import MainPage from './ui-components/MainPage';
+import "@aws-amplify/ui-react/styles.css";
 import logo2 from './logo_2.png';
-import logo3 from './logo.svg'
+import logo from './logo.svg'
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+ 
+ <MainPage />
 
-function App() {
-
+/*function App() {
+  
   const GoodSignUpOverrides = {
     "Logo": {
-      path: "Users/Ptwo1127/Desktop/src/logo_2.png" // Replace this path with the path to your new logo image
+      path: "./logo.svg" // Replace this path with the path to your new logo image
     }
   };
 
   return (
     <div className="App">
-      {/* Include the GoodSignUp component with the logo prop */}
+      {/* Include the GoodSignUp component with the logo prop }
       <GoodSignUp overrides={GoodSignUpOverrides} />
     </div>
   );
 }
 
-export default App;
+export default App;*/
+
+function App({ signOut }) {
+  return (
+    <div className="App">
+       <MainPage />
+       <Button onClick={signOut}>Sign Out</Button>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
+
+
 
